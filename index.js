553c4e3f9a2302fb38388feb08b9cfbd7ad0db19
monica <monica.kwon@kakaocorp.com>
2018-10-25 10:36:42 +0900

    Fix port

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var fs = require("fs")
-var port = process.ENV.PORT || 80
+var port = process.env.PORT || 80
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 var server = app.listen(port, function(){
-    console.log("Express server has started on port 3000")
+    console.log("Express server has started on port " + port)
 });
 
 app.use(express.static('public'));
